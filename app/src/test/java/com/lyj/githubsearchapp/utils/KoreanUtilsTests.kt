3c3f22fb028167f9355b5299fa4eb825a6b4de02
iyeongjun <they6687@naver.com>
2021-10-17 23:18:17 +0900

    Test: 초성 분해 유틸 테스트
    - 모음으로 시작하는 경우 에러
    - 테스트 통과

---
+++ app/src/test/java/com/lyj/githubsearchapp/utils/KoreanUtilsTests.kt
@@ -0,0 +1,49 @@
+package com.lyj.githubsearchapp.utils
+
+import com.lyj.githubsearchapp.common.utils.InitialSoundNotCorrectedException
+import com.lyj.githubsearchapp.common.utils.KoreanLangagueUtils
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [29])
+class KoreanUtilsTests{
+    private val correctTextList = listOf<Pair<Char,String>>(
+        'ㅂ' to "반지하",
+        'b' to "ban",
+        'ㅎ' to "하지반",
+        'ㅈ' to "지하반",
+        'a' to "anb",
+        'n' to "nba"
+    )
+
+    private val incrrectText = "ㅏ에이오우"
+
+
+    @Test
+    fun `초성_분해_테스트_옳은_예`(){
+        correctTextList
+            .forEach { (char, str)->
+                try {
+                    val (i, s) = KoreanLangagueUtils.splitInitialSound(str)
+                    println("$i $s")
+                    assert(i == char)
+                }catch (e : InitialSoundNotCorrectedException){
+                    assert(false)
+                }
+            }
+    }
+    @Test
+    fun `초성_분해_테스트_틀린_예`(){
+        try {
+            KoreanLangagueUtils.splitInitialSound(incrrectText)
+            assert(false)
+        }catch (e : InitialSoundNotCorrectedException){
+            assert(true)
+        }
+    }
+
+}
